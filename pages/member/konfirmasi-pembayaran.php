e412a9eccb82310664c7b95be6431e7fde9b083f
Ego Dafma Dasa <egodasa@gmail.com>
2019-12-26 15:02:42 +0700

    Memindahkan js ajax konfirmasi agar tidak perlu edit base url pada js

--- pages/member/konfirmasi-pembayaran.php
+++ pages/member/konfirmasi-pembayaran.php
@@ -122,4 +122,42 @@
    </div>
    <!-- End Cart -->
 
-   <script src="../asset/js/konfirmasi.js"></script>
+   <script>
+   function cek_no_order() {
+      var id_js = document.getElementById("noorder").value;
+      var json_url = "json/tampil-order.php?id_js="+id_js;
+    
+      var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+           // Typical action to be performed when the document is ready:
+           var myArr = JSON.parse(this.responseText);
+    
+    
+            if (myArr.length == 0) 
+             {
+             
+                console.log("madam")
+             }
+             else{
+    
+              for(i = 0; i < myArr.length; i++) 
+              {
+                alert("NO Order : "+myArr[i][0].idorder+"\n"+"Nama : "+myArr[i][0].namaorder+"\n"+"Status : "+myArr[i][0].status  );
+                document.getElementById("namaorder").innerHTML=": " +myArr[i][0].namaorder;
+                document.getElementById("tglorder").innerHTML=": " +myArr[i][0].tglorder;
+                document.getElementById("status_ord").innerHTML=": " +myArr[i][0].status;
+                document.getElementById("totalbelanja").innerHTML=": " +myArr[i][0].totalbelanja;transaksi_id 
+                document.getElementById("transaksi_id").value=myArr[i][0].idorder;
+              }
+    
+             }
+        }
+      };
+      
+      xhttp.open("GET", json_url, true);
+      xhttp.send();
+    }
+   </script>
